0e7949968df61cd0bc2a957e54bbeb73f9be8967
Kimkopp <kimkop1@hotmail.com>
2021-11-04 21:33:14 +0100

    test

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,8 +1,8 @@
 public class LeapYear {
-    //
+
     static boolean isLeapYear(int year) {
         if (year % 400 != 0 && year % 100 == 0) {
             return false;
         } else return year % 4 == 0;
     }
-} //
+}
